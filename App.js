ad4e055c0e1870c71fd6057e553c97d0816f0556
Evgeniy Akulenko <akulenko.contact@gmail.com>
2025-01-07 15:24:48 +0300

    add Icons in drawer

--- App.js
+++ App.js
@@ -3,6 +3,7 @@ import { StyleSheet } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import { createDrawerNavigator } from '@react-navigation/drawer';
+import { Ionicons } from '@expo/vector-icons';
 
 import CategoriesScreen from './screens/CategoriesScreen';
 import MealsOverviewScreen from './screens/MealsOverviewScreen';
@@ -19,14 +20,31 @@ function DrawerNavigator() {
                 headerStyle: { backgroundColor: '#351401' },
                 headerTintColor: 'white',
                 sceneStyle: { backgroundColor: '#3f2f25' },
+                drawerContentStyle: { backgroundColor: '#351401' },
+                drawerInactiveTintColor: 'white',
+                drawerActiveTintColor: '#351401',
+                drawerActiveBackgroundColor: '#e4baa1',
             }}
         >
             <Drawer.Screen
                 name="Categoriies"
                 component={CategoriesScreen}
-                options={{ title: 'All Categories' }}
+                options={{
+                    title: 'All Categories',
+                    drawerIcon: ({ color, size }) => (
+                        <Ionicons name="list" color={color} size={size} />
+                    ),
+                }}
             />
-            <Drawer.Screen name="Favorite" component={FavoriteScreen} />
+            <Drawer.Screen
+                name="Favorite"
+                component={FavoriteScreen}
+                options={{
+                    drawerIcon: ({ color, size }) => (
+                        <Ionicons name="star" color={color} size={size} />
+                    ),
+                }}
+            />
         </Drawer.Navigator>
     );
 }
